f793ca868eb15e5829a848e3d0762ffe9d4fe181
Nimo <nimo.jser@gmail.com>
2017-11-30 15:33:23 +0800

    ajax

---
+++ s.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var serve   = require('express-static')
+var app = express()
+var open = require("open");
+app.use(serve(__dirname))
+var port = 8269
+var url = "http://127.0.0.1:" + String(port)
+app.listen(port, function () {
+    console.log('open ' + url)
+    open(url)
+})
